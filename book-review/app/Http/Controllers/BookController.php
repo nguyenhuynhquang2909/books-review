198408f18a77a93a2fb0ce8028884fdedf55a171
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2024-06-21 15:28:13 +0700

    Update caching queries

--- book-review/app/Http/Controllers/BookController.php
+++ book-review/app/Http/Controllers/BookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Book;
 use Illuminate\Http\Request;
 
+
 class BookController extends Controller
 {
     /**
@@ -13,11 +14,20 @@ class BookController extends Controller
     public function index(Request $request)
     {
         $title = $request->input('title');
+        $filter = $request->input('filter', '');
         $books = Book::when(
             $title,
             fn($query, $title) => $query->title($title)
-        )
-            ->get();
+        );
+        $books = match($filter) {
+            'popular_last_month' =>$books->popularLastMonth(),
+            'popular_last_6months' =>$books->popularLast6Months(),
+            'highest_rated_last_month' =>$books->highestRatedLastMonth(),
+            'highest_rated_last_6months' =>$books->highestRatedLast6Months(),
+            default => $books->latest()
+        };
+        $cacheKey = "books:" .$filter  . ':' . $title;
+        $books = cache()->remember($cacheKey, 3600, fn() => $books->get());
         return view('books.index', ['books' => $books]);
     }
 
@@ -40,9 +50,15 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Book $book) 
     {
-        //
+        $cachekey = 'book:' .$book->id;
+        $book = cache()->remember($cachekey, 3600, fn() => $book -> load(
+            [
+                'reviews' => fn($query) => $query->latest(),
+            ]
+            ));
+        return view('books.show', ['book' => $book ]);
     }
 
     /**
